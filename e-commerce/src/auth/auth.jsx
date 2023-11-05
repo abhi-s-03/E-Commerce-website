d12f8afd0dc490c1bcc811f6263ac1a41570180d
abhi-s-03 <abhisheks3002@gmail.com>
2023-11-05 23:31:53 +0530

    login update

--- e-commerce/src/auth/auth.jsx
+++ e-commerce/src/auth/auth.jsx
@@ -3,13 +3,14 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
+  apiKey: "AIzaSyAjCBgYjLkVHH9egP-klxA95wzXNdCOln4",
   authDomain: "e-commerce-website-4c47d.firebaseapp.com",
   projectId: "e-commerce-website-4c47d",
   storageBucket: "e-commerce-website-4c47d.appspot.com",
   messagingSenderId: "77440245018",
-  appId: "1:77440245018:web:8c243d81abfe3fc3c520a3",
-  measurementId: "G-W5HFRPJ9HK",
+  appId: "1:77440245018:web:c370caa40a2c3259c520a3",
+  measurementId: "G-7RJGHS8P5S"
+
 };
 
 const app = initializeApp(firebaseConfig);
