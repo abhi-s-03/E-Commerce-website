4269a0bd071b083f72df2dff2f535a118f70e905
abhi-s-03 <abhisheks3002@gmail.com>
2023-11-07 07:02:35 +0530

    navbar

--- e-commerce/src/pages/landing.jsx
+++ e-commerce/src/pages/landing.jsx
@@ -1,11 +1,8 @@
-import { useNavigate } from "react-router-dom";
-
+import Navbar from "../components/navbar";
 const Landing = () => {
-  const navigate=useNavigate();
   return (
     <div>
-      <h1>Landing Page</h1>
-      <button onClick={()=>navigate("/login")}>Signup</button>
+    <Navbar />
     </div>
   );
 };
