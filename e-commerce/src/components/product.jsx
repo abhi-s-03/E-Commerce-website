734329dee450b2f36f1379121083da54c41b928b
abhi-s-03 <abhisheks3002@gmail.com>
2023-11-07 11:43:55 +0530

    offer and product added

---
+++ e-commerce/src/components/product.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import "./styles/product.css";
+
+const Product = ({ heading, products }) => {
+  return (
+    <div className="products-outer">
+      <h2>{heading}</h2>
+      <div className="products">
+        {products.map((product, index) => (
+          <div className="product" key={index}>
+            <Link to={product.link}>
+              <img src={product.image} alt={product.title} />
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Product;
--- e-commerce/src/pages/landing.jsx
+++ e-commerce/src/pages/landing.jsx
@@ -1,10 +1,47 @@
 import Navbar from "../components/navbar";
 import Offers from "../components/offers";
+import Product from "../components/product";
+import painting1 from "../assets/painting1.jpeg";
+import painting2 from "../assets/painting2.jpeg";
+import painting3 from "../assets/painting3.jpeg";
+import painting4 from "../assets/painting4.jpeg";
+import painting5 from "../assets/painting5.jpeg";
+import "./styles/landing.css";
+
+const painting = [
+  {
+    link: "/",
+    image: painting1,
+    title: "Product 1",
+  },
+  {
+    link: "/",
+    image: painting2,
+    title: "Product 2",
+  },
+  {
+    link: "/",
+    image: painting3,
+    title: "Product 3",
+  },
+  {
+    link: "/",
+    image: painting4,
+    title: "Product 4",
+  },
+  {
+    link: "/",
+    image: painting5,
+    title: "Product 5",
+  },
+];
+
 const Landing = () => {
   return (
-    <div>
-    <Navbar />
-    <Offers />
+    <div className="landing">
+      <Navbar />
+      <Offers />
+      <Product heading="Paintings" products={painting} />
     </div>
   );
 };
