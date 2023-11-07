850db96b5d44a8514c582c9addc0c8e6e854135a
rajath-tk <rajath.kurian@gmail.com>
2023-11-08 01:46:53 +0530

    Added Edit art functionality

--- e-commerce/src/components/ArtworkManagement.jsx
+++ e-commerce/src/components/ArtworkManagement.jsx
@@ -6,32 +6,81 @@ import {
   getDocs,
   where,
   query,
+  updateDoc,
+  doc
 } from "firebase/firestore";
 import {db} from "../auth/auth";
 
 function ArtworkManagement() {
   const [artistProd, setArtistProd] = useState([]);
+  const [editArtworkData, setEditArtworkData] = useState({
+    id: '',
+    prodName: '',
+    prodDesc: '',
+    prodQty: 0,
+    prodPrice: 0,
+  });
+  const [isEditing, setIsEditing] = useState(false);
+
+  const loadProducts = async () => {
+    try {
+      const q = query(collection(db, "products"), where("artistID", "==", "Aaron"));
+      const snapshot = await getDocs(q);
+      const prodArray = [];
+      snapshot.forEach((doc) => {
+        prodArray.push({
+          id: doc.id, // Add the document ID to the object
+          ...doc.data(), // Spread the document data
+        });
+      });
+      setArtistProd(prodArray);
+    } catch (error) {
+      console.error("ERROR is" + error);
+    }
+  };
 
   useEffect(() => {
-    const loadProducts = async () => {
-      try {
-        const q = query(collection(db, "products"), where("artistID", "==", "Aaron"));
-        const snapshot = await getDocs(q);
-        const prodArray = [];
-        snapshot.forEach((doc) => {
-          prodArray.push(doc.data());
-        });
-        setArtistProd(prodArray);
-      } catch (error) {
-        console.error("ERROR is" + error);
-      }
-    };
-
     loadProducts();
   }, []); // Empty dependency array to run the effect only once on component mount
 
   const editArtwork = (id) => {
-    // Implement functionality to edit an existing artwork
+    const artworkToEdit = artistProd.find((art) => art.id === id);
+    console.log(id);
+    setEditArtworkData(artworkToEdit);
+    setIsEditing(true);
+  };
+
+  const cancelEdit = () => {
+    setEditArtworkData({
+      id: '',
+      prodName: '',
+      prodDesc: '',
+      prodQty: 0,
+      prodPrice: 0,
+    });
+    setIsEditing(false);
+  };
+
+  const saveEdit = async () => {
+    const { id, prodName, prodDesc, prodQty, prodPrice } = editArtworkData;
+    console.log(id);
+
+    // Update the artwork data in Firestore
+    try {
+      //const artworkDocRef = doc(db, 'products', id);
+      await updateDoc(doc(db, 'products', id), {
+        prodName,
+        prodDesc,
+        prodQty: parseInt(prodQty),
+        prodPrice: parseInt(prodPrice),
+      });
+
+      // After successfully updating, reload the products and exit edit mode
+      loadProducts();
+      cancelEdit();
+    } catch (error) {
+      console.error('Error updating artwork:', error);
+    }
   };
 
   const deleteArtwork = (id) => {
@@ -62,6 +111,48 @@ function ArtworkManagement() {
           </div>
         ))}
       </div>
+
+      {/* This is the edit art component */}
+      {isEditing && (
+        <div className="edit-artwork-form">
+          <h3>Edit Artwork</h3>
+          <label>
+            Name:
+            <input
+              type="text"
+              value={editArtworkData.prodName}
+              onChange={(e) => setEditArtworkData({ ...editArtworkData, prodName: e.target.value })}
+            />
+          </label>
+          <label>
+            Description:
+            <textarea
+              value={editArtworkData.prodDesc}
+              onChange={(e) => setEditArtworkData({ ...editArtworkData, prodDesc: e.target.value })}
+            />
+          </label>
+          <label>
+            Quantity:
+            <input
+              type="number"
+              value={editArtworkData.prodQty}
+              onChange={(e) => setEditArtworkData({ ...editArtworkData, prodQty: parseInt(e.target.value) || 0 })}
+            />
+          </label>
+          <label>
+            Price:
+            <input
+              type="number"
+              value={editArtworkData.prodPrice}
+              onChange={(e) => setEditArtworkData({ ...editArtworkData, prodPrice: parseFloat(e.target.value) || 0 })}
+            />
+          </label>
+          <div className="edit-artwork-buttons">
+            <button onClick={saveEdit}>Save</button>
+            <button onClick={cancelEdit}>Cancel</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
