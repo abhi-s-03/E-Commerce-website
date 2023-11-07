f01509ac23e025b148ea49cd6ed0b44e63d170f3
abhi-s-03 <abhisheks3002@gmail.com>
2023-11-07 13:14:23 +0530

    Add handicraft products to landing page and update background color

--- e-commerce/src/pages/landing.jsx
+++ e-commerce/src/pages/landing.jsx
@@ -21,6 +21,11 @@ import photo2 from "../assets/photo2.jpeg";
 import photo3 from "../assets/photo3.jpeg";
 import photo4 from "../assets/photo4.jpeg";
 import photo5 from "../assets/photo5.jpeg";
+import handicraft1 from "../assets/handicraft1.jpeg";
+import handicraft2 from "../assets/handicraft2.jpeg";
+import handicraft3 from "../assets/handicraft3.jpeg";
+import handicraft4 from "../assets/handicraft4.jpeg";
+import handicraft5 from "../assets/handicraft5.jpeg";
 import "./styles/landing.css";
 
 const painting = [
@@ -135,6 +140,34 @@ const photo = [
   },
 ];
 
+const handicraft = [
+  {
+    link: "/",
+    image: handicraft1,
+    title: "Product 1",
+  },
+  {
+    link: "/",
+    image: handicraft2,
+    title: "Product 2",
+  },
+  {
+    link: "/",
+    image: handicraft3,
+    title: "Product 3",
+  },
+  {
+    link: "/",
+    image: handicraft4,
+    title: "Product 4",
+  },
+  {
+    link: "/",
+    image: handicraft5,
+    title: "Product 5",
+  },
+];
+
 const Landing = () => {
   return (
     <div className="landing">
@@ -144,6 +177,7 @@ const Landing = () => {
       <Product heading="Sculptures" products={sculpture} />
       <Product heading="Digital Art" products={digiart} />
       <Product heading="Photography" products={photo} />
+      <Product heading="Handicrafts" products={handicraft} />
     </div>
   );
 };
