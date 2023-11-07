a8c88d966e2757bc8450771cd26030e9e9d7f2d2
Aaron Jacob <aaronjacobaj4@gmail.com>
2023-11-07 09:33:51 +0530

    fixed some sizing issues in cart page

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -2,19 +2,23 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import LoginForm from "./pages/login";
 import SignupForm from "./pages/signup";
+import Cart from "./pages/cart";
 import Landing from "./pages/landing"
 
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Landing/>} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/signup" element={<SignupForm />} />
-        </Routes>
-      </Router>
-    </>
+    //<>
+      //<Router>
+        //<Routes>
+          //<Route path="/" element={<Landing/>} />
+          //<Route path="/login" element={<LoginForm />} />
+          //<Route path="/signup" element={<SignupForm />} />
+        //</Routes>
+      //</Router>
+      <div className="App">
+      <Cart/>
+    </div>
+    //</>
   );
 }
 
