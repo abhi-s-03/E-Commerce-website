bb4f32ffb85a978cbba46eaef1e9528d938fd333
abhi-s-03 <abhisheks3002@gmail.com>
2023-11-07 18:48:04 +0530

    Refactor SellerDashboard component

--- e-commerce/src/pages/SellerDashboard.jsx
+++ e-commerce/src/pages/SellerDashboard.jsx
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import './styles/SellerDashboard.css';
-import ArtworkManagement from './sellerdash/ArtworkManagement';
-import Orders from './sellerdash/Orders';
-import Profile from './sellerdash/Profile';
-import Earnings from './sellerdash/Earnings';
+import { useState } from "react";
+import "./styles/SellerDashboard.css";
+import ArtworkManagement from "./sellerdash/ArtworkManagement";
+import Orders from "./sellerdash/Orders";
+import Profile from "./sellerdash/Profile";
+import Earnings from "./sellerdash/Earnings";
 import { Link } from "react-router-dom";
 
 const SellerDashboard = () => {
-  const [activeSection, setActiveSection] = useState('artworkManagement');
+  const [activeSection, setActiveSection] = useState("artworkManagement");
 
   const handleSectionChange = (section) => {
     setActiveSection(section);
@@ -18,7 +18,10 @@ const SellerDashboard = () => {
       <header>
         <h1>Seller Dashboard</h1>
         <Link to="/">
-          <button onClick={() => window.location.href = '/'} className="return-button">
+          <button
+            onClick={() => (window.location.href = "/")}
+            className="return-button"
+          >
             Return
           </button>
         </Link>
@@ -27,36 +30,36 @@ const SellerDashboard = () => {
         <nav className="side-menu">
           <ul>
             <li
-              onClick={() => handleSectionChange('artworkManagement')}
-              className={activeSection === 'artworkManagement' ? 'active' : ''}
+              onClick={() => handleSectionChange("artworkManagement")}
+              className={activeSection === "artworkManagement" ? "active" : ""}
             >
               Artwork Management
             </li>
             <li
-              onClick={() => handleSectionChange('orders')}
-              className={activeSection === 'orders' ? 'active' : ''}
+              onClick={() => handleSectionChange("orders")}
+              className={activeSection === "orders" ? "active" : ""}
             >
               Orders
             </li>
             <li
-              onClick={() => handleSectionChange('profile')}
-              className={activeSection === 'profile' ? 'active' : ''}
+              onClick={() => handleSectionChange("profile")}
+              className={activeSection === "profile" ? "active" : ""}
             >
               Profile
             </li>
             <li
-              onClick={() => handleSectionChange('earnings')}
-              className={activeSection === 'earnings' ? 'active' : ''}
+              onClick={() => handleSectionChange("earnings")}
+              className={activeSection === "earnings" ? "active" : ""}
             >
               Earnings
             </li>
           </ul>
         </nav>
         <div className="section-content">
-          {activeSection === 'artworkManagement' && <ArtworkManagement />}
-          {activeSection === 'orders' && <Orders />}
-          {activeSection === 'profile' && <Profile />}
-          {activeSection === 'earnings' && <Earnings />}
+          {activeSection === "artworkManagement" && <ArtworkManagement />}
+          {activeSection === "orders" && <Orders />}
+          {activeSection === "profile" && <Profile />}
+          {activeSection === "earnings" && <Earnings />}
         </div>
       </div>
     </div>
