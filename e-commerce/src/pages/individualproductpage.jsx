4195a2fed695c741921a872f94db9820c3198a01
Aaron Jacob <aaronjacobaj4@gmail.com>
2023-11-10 13:59:27 +0530

    Individual Product Page Added

---
+++ e-commerce/src/pages/individualproductpage.jsx
@@ -0,0 +1,47 @@
+// individualproductpage.jsx
+
+import React from "react";
+import { Link } from "react-router-dom";
+import carticon from "../assets/carticon.svg";
+import backArrow from "../assets/back-arrow.svg"; 
+
+import './styles/individualproductpage.css';
+
+function IndividualProductPage({ location = { state: null } }) {
+  // Dummy values
+  const dummyData = {
+    sellerName: "Abhishek S",
+    image: "item1.jpg", // Placeholder image URL
+    prodName: "Abstract Masterpiece",
+    prodDesc: "A stunning abstract artwork that captures the essence of emotions. Remembering to Abhilash for his new Ray Ban glasses",
+    prodPrice: 200.50,
+  };
+
+  const { sellerName, image, prodName, prodDesc, prodPrice } = location.state || dummyData;
+
+  return (
+    <div className="individual-product-container">
+      <Link to="#" className="back-link">
+        {/* Use your arrow icon or simply use '<' */}
+        <img src={backArrow} alt="Back" className="back-arrow" />
+      </Link>
+      <div className="product-image-container">
+        <img src={image} alt={prodName} className="product-image" />
+      </div>
+      <div className="product-details-container">
+        <h2 className="product-name">{prodName}</h2>
+        <p className="seller-name">Sold by: {sellerName}</p>
+        <p className="product-price">Price: ₹{prodPrice}</p>
+        <p className="product-description">{prodDesc}</p>
+        <div className="add-to-cart-section">
+          <button className="add-to-cart-button">
+            <img src={carticon} alt="Add to Cart" className="cart-icon" />
+            Add to Cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default IndividualProductPage;
