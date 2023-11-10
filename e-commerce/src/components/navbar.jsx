d26300c5a4999f36bcd6e03be2b4725aa5c522cd
Aaron Jacob <aaronjacobaj4@gmail.com>
2023-11-10 15:07:33 +0530

    minor change

--- e-commerce/src/components/navbar.jsx
+++ e-commerce/src/components/navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
       <div className="topbar-right">
         <ul className="topbar-links">
           <li>
-            <Link to="/individual">Seller</Link>
+            <Link to="/seller">Seller</Link>
           </li>
           <li>
             <Link to="/login">Sign In</Link>
