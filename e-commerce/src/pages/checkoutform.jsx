651741ad05b550aec8b19a9ce0da863c01ebe737
Aaron Jacob <aaronjacobaj4@gmail.com>
2023-11-10 19:32:46 +0530

    checkout and order summary UI improved

--- e-commerce/src/pages/checkoutform.jsx
+++ e-commerce/src/pages/checkoutform.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
-import "./styles/checkout.css";
-import leftarrow from "../assets/leftarrow.svg";
-import { Link } from "react-router-dom";
+// CheckoutForm.jsx
 
-function Checkout() {
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./styles/CheckoutForm.css";
+import backArrow from "../assets/back-arrow.svg";
+import rightArrow from "../assets/right-arrow.svg";
+
+function CheckoutForm() {
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
@@ -15,44 +18,9 @@ function Checkout() {
     email: "",
     phoneNumber: "",
     paymentOption: "",
-    cardName: "",
-    cardNumber: "",
-    expiryDate: "",
   });
 
-  const [cartItems, setCartItems] = useState([
-    {
-      id: 1,
-      name: "Product 1",
-      price: 50,
-      quantity: 2,
-    },
-    {
-      id: 2,
-      name: "Product 2",
-      price: 30,
-      quantity: 1,
-    },
-  ]);
-
-  const calculateSubtotal = () => {
-    return cartItems.reduce(
-      (total, item) => total + item.price * item.quantity,
-      0
-    );
-  };
-
-  const calculateShippingCharges = () => {
-    // Implement shipping charges calculation logic here
-    return 10;
-  };
-
-  const calculateFulltotal = () => {
-    return (
-      cartItems.reduce((total, item) => total + item.price * item.quantity, 0) +
-      calculateShippingCharges()
-    );
-  };
+  const [error, setError] = useState("");
 
   const handleFormChange = (e) => {
     const { name, value } = e.target;
@@ -60,98 +28,121 @@ function Checkout() {
       ...prevData,
       [name]: value,
     }));
+
+    // Clear the error message when a field is changed
+    setError("");
   };
 
-  const handlePayment = () => {
-    // Implement payment processing logic here
+  const handleProceed = () => {
+    // Validate form fields before proceeding
+    const isFormValid = Object.values(formData).every((value) => value.trim() !== "");
+
+    if (isFormValid) {
+      // Implement logic for handling the "Proceed" button
+      // You can add additional validation or processing here
+      console.log("Proceed button clicked!");
+    } else {
+      setError("Fill all fields");
+    }
   };
 
   return (
     <div className="checkout-container">
-      <div className="checkout-container-header">Art Mart</div>
+      <div className="checkout-container-header">CHECKOUT</div>
 
       <div className="checkout-form">
         <form>
           <div className="form-row">
             <div className="form-group">
-              <label htmlFor="firstName">First Name:</label>
               <input
                 type="text"
                 name="firstName"
                 value={formData.firstName}
                 onChange={handleFormChange}
+                placeholder="First Name"
               />
             </div>
             <div className="form-group">
-              <label htmlFor="lastName">Last Name:</label>
               <input
                 type="text"
                 name="lastName"
                 value={formData.lastName}
                 onChange={handleFormChange}
+                placeholder="Last Name"
+              />
+            </div>
+          </div>
+          <div className="form-row">
+            <div className="form-group full-width">
+              <input
+                type="text"
+                name="address"
+                value={formData.address}
+                onChange={handleFormChange}
+                placeholder="Address"
               />
             </div>
           </div>
           <div className="form-row">
             <div className="form-group">
-              <label htmlFor="country">Country:</label>
               <input
                 type="text"
                 name="country"
                 value={formData.country}
                 onChange={handleFormChange}
+                placeholder="Country"
               />
             </div>
             <div className="form-group">
-              <label htmlFor="region">Region/State:</label>
               <input
                 type="text"
                 name="region"
                 value={formData.region}
                 onChange={handleFormChange}
+                placeholder="Region/State"
               />
             </div>
             <div className="form-group">
-              <label htmlFor="city">City:</label>
               <input
                 type="text"
                 name="city"
                 value={formData.city}
                 onChange={handleFormChange}
+                placeholder="City"
               />
             </div>
             <div className="form-group">
-              <label htmlFor="zipCode">Zip Code:</label>
               <input
                 type="text"
                 name="zipCode"
                 value={formData.zipCode}
                 onChange={handleFormChange}
+                placeholder="Zip Code"
               />
             </div>
           </div>
           <div className="form-row">
             <div className="form-group">
-              <label htmlFor="email">E-mail:</label>
               <input
                 type="email"
                 name="email"
                 value={formData.email}
                 onChange={handleFormChange}
+                placeholder="E-mail"
               />
             </div>
             <div className="form-group">
-              <label htmlFor="phoneNumber">Phone Number:</label>
               <input
                 type="text"
                 name="phoneNumber"
                 value={formData.phoneNumber}
                 onChange={handleFormChange}
+                placeholder="Phone Number"
               />
             </div>
           </div>
-          <div className="form-group">
-            <label>Payment Option:</label>
+          <div className="form-group payment-options full-width">
+            <div className="subheading">Payment Option</div>
             <div className="radio-group">
               <label>
                 UPI
@@ -185,82 +176,25 @@ function Checkout() {
               </label>
             </div>
           </div>
-
-          <div className="form-row">
-            <div className="form-group">
-              <label htmlFor="cardName">Name on Card:</label>
-              <input
-                type="text"
-                name="cardName"
-                value={formData.cardName}
-                onChange={handleFormChange}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="cardNumber">Card Number:</label>
-              <input
-                type="text"
-                name="cardNumber"
-                value={formData.cardNumber}
-                onChange={handleFormChange}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="expiryDate">Expiry Date:</label>
-              <input
-                type="text"
-                name="expiryDate"
-                value={formData.expiryDate}
-                onChange={handleFormChange}
-              />
-            </div>
-          </div>
         </form>
         <div className="checkout-child" />
         <div className="return-from-checkout">
-          <button className="return-from-checkout-button">
+          <Link to="/cart" className="back-link">
             <img
-              className="return-from-checkout-regulararrowleft-icon"
+              className="back-arrow"
               alt=""
-              src={leftarrow}
+              src={backArrow}
             />
-            <Link to="/cart">
-              <b className="return-from-checkout-label4"> Return to Cart</b>
-            </Link>
+          </Link>
+        </div>
+        <Link to="/ordersummary" className="proceed-button-link">
+          <button className="proceed-button" onClick={handleProceed}>
+            PROCEED <img src={rightArrow} alt="Right Arrow" className="arrow-icon" />
           </button>
-        </div>
-      </div>
-
-      <div className="order-summary">
-        <h2 className="order-summary-heading">Order Summary</h2>
-        <div className="product-details">
-          {cartItems.map((item) => (
-            <div className="product-name" key={item.id}>
-              <div className="product1-name">{item.name}</div>
-              <span>Sub-total: ₹{item.price * item.quantity}.00</span>
-            </div>
-          ))}
-        </div>
-        <div className="order-details">
-          <div className="subtotal">
-            <span>Sub-Total:</span>
-            <span>₹{calculateSubtotal()}.00</span>
-          </div>
-          <div className="shipping-charges">
-            <span>Shipping Charges:</span>
-            <span>₹{calculateShippingCharges()}.00</span>
-          </div>
-          <div className="total-amount">
-            <span>Total Amount:</span>
-            <span>₹{calculateFulltotal()}.00</span>
-          </div>
-        </div>
-        <button className="place-order-button" onClick={handlePayment}>
-          Place Order
-        </button>
+        </Link>
       </div>
     </div>
   );
 }
 
-export default Checkout;
+export default CheckoutForm;
