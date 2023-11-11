b74b85a013bd1511108c6dad48800851207dd581
abhi-s-03 <abhisheks3002@gmail.com>
2023-11-11 16:06:01 +0530

    Update seller and cart page styles

--- e-commerce/src/pages/cart.jsx
+++ e-commerce/src/pages/cart.jsx
@@ -1,33 +1,28 @@
-import { useState ,useEffect} from "react";
+import { useState, useEffect } from "react";
 import "./styles/cart.css";
 import deleteimg from "../assets/delete-img.svg";
 import plussymbol from "../assets/plus-symbol.svg";
 import minussymbol from "../assets/minus-symbol.svg";
-import leftarrow from "../assets/leftarrow.svg";
 import { Link } from "react-router-dom";
-import {
-  collection,
-  getDocs,
-  where,
-  query, 
-  updateDoc,
-  doc,
-  deleteDoc,
-} from "firebase/firestore";
+import { collection, getDocs, where, query } from "firebase/firestore";
 import { db } from "../auth/auth";
+import Navbar from "../components/Navbar";
 
 function Cart() {
   const [cartItems, setCartItems] = useState([]);
   const [productMap, setProductMap] = useState({});
-  
+
   useEffect(() => {
     const loadProducts = async () => {
       try {
-        const q1 = query(collection(db, "carts"), where("userID", "==", "Admin"));
+        const q1 = query(
+          collection(db, "carts"),
+          where("userID", "==", "Admin")
+        );
         const q2 = query(collection(db, "products"));
         const productSnapshot = await getDocs(q2);
         const cartSnapshot = await getDocs(q1);
-        
+
         const cartArray = cartSnapshot.docs.map((doc) => ({
           id: doc.id,
           ...doc.data(),
@@ -73,16 +68,12 @@ function Cart() {
     setCartItems(updatedCart);
   };
 
- 
-
   const calculateSubtotal = () => {
     return cartItems.reduce(
       (total, item) => total + item.price * item.quantity,
       0
     );
   };
-
-
 
   const calculateFulltotal = () => {
     return (
@@ -92,121 +83,112 @@ function Cart() {
   };
 
   return (
-    <div className="cart-container">
-      <div className="cart-container-header">Art Mart</div>
+    <>
+      <Navbar />
+      <div className="cart-container">
+        <div className="cart-container-Product-list">
+          <div className="Product-list-heading">
+            <div className="heading-shopping-cart1">Shopping Cart</div>
+          </div>
+          <div className="Product-list-sub-heading">
+            <div className="sub-heading-products">Products</div>
+            <div className="sub-heading-price">Price</div>
+            <div className="sub-heading-quantity1">Quantity</div>
+            <div className="sub-heading-sub-total">Sub-Total</div>
+          </div>
 
-      <div className="cart-container-Product-list">
-        <div className="Product-list-heading">
-          <div className="heading-shopping-cart1">Shopping Cart</div>
-        </div>
-        <div className="Product-list-sub-heading">
-          <div className="sub-heading-products">Products</div>
-          <div className="sub-heading-price">Price</div>
-          <div className="sub-heading-quantity1">Quantity</div>
-          <div className="sub-heading-sub-total">Sub-Total</div>
-        </div>
+          <div className="Product-list-cart-items">
+            {cartItems.map((item) => (
+              <div className="cart-items-cart-item" key={item.id}>
+                <div className="cart-item-cart-item1">
+                  <button
+                    onClick={() => removeItem(item.id)}
+                    className="cart-item1-remove-button"
+                  >
+                    <img
+                      className="remove-button-delete-icon"
+                      alt=""
+                      src={deleteimg}
+                    />
+                  </button>
 
-        <div className="Product-list-cart-items">
-          {cartItems.map((item) => (
-            <div className="cart-items-cart-item" key={item.id}>
-              <div className="cart-item-cart-item1">
-                <button
-                  onClick={() => removeItem(item.id)}
-                  className="cart-item1-remove-button"
-                >
                   <img
-                    className="remove-button-delete-icon"
-                    alt=""
-                    src={deleteimg}
+                    className="cart-item1-item-image"
+                    src={productMap[item.productID].image}
+                    alt={"ss"}
                   />
-                </button>
-                
-                <img
-                className="cart-item1-item-image"
-                src={productMap[item.productID].image}
-                alt={"ss"}
-              />
 
-                <div className="cart-item1-item-name">{productMap[item.productID].prodName}</div>
-              </div>
+                  <div className="cart-item1-item-name">
+                    {productMap[item.productID].prodName}
+                  </div>
+                </div>
 
-              <div className="cart-item-item-price">{productMap[item.productID].prodPrice}</div>
+                <div className="cart-item-item-price">
+                  {productMap[item.productID].prodPrice}
+                </div>
 
-              <div className="cart-item-quantity">
-                <div className="quantity-button2">
-                  <button
-                    onClick={() => decreaseQuantity(item.id)}
-                    className="decrbutt"
-                  >
-                    <img
-                      className="button2-minus-icon"
-                      alt=""
-                      src={minussymbol}
-                    />
-                  </button>
-                  <div className="button2-item-count">{item.quantity}</div>
-                  <button
-                    onClick={() => increaseQuantity(item.id)}
-                    className="incrbutt"
-                  >
-                    <img
-                      className="button2-plus-icon"
-                      alt=""
-                      src={plussymbol}
-                    />
-                  </button>
+                <div className="cart-item-quantity">
+                  <div className="quantity-button2">
+                    <button
+                      onClick={() => decreaseQuantity(item.id)}
+                      className="decrbutt"
+                    >
+                      <img
+                        className="button2-minus-icon"
+                        alt=""
+                        src={minussymbol}
+                      />
+                    </button>
+                    <div className="button2-item-count">{item.quantity}</div>
+                    <button
+                      onClick={() => increaseQuantity(item.id)}
+                      className="incrbutt"
+                    >
+                      <img
+                        className="button2-plus-icon"
+                        alt=""
+                        src={plussymbol}
+                      />
+                    </button>
+                  </div>
+                </div>
+                <div className="cart-item-item-subtotal">
+                  ₹{productMap[item.productID].prodPrice * item.quantity}.00
                 </div>
               </div>
-              <div className="cart-item-item-subtotal">
-                ₹{productMap[item.productID].prodPrice * item.quantity}.00
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
 
-        <div className="Product-list-shopping-cart-child" />
-        <div className="Product-list-return-from-cart-section">
-          <button className="return-from-cart-section-return-from-cart">
-            <img
-              className="return-from-cart-regulararrowleft-icon"
-              alt=""
-              src={leftarrow}
-            />
-            <Link to="/">
-              <b className="return-from-cart-label4"> Return to Shop</b>
+        <div className="cart-container-cart-total">
+          <div className="cart-total-heading">
+            <div className="heading-cart-totals">Cart Totals</div>
+          </div>
+          <div className="cart-total-money">
+            <div className="sub-total-parent">
+              <div className="sub-total-child">
+                <div className="subtotal-label">Sub-total</div>
+                <div className="subtotal">₹{calculateSubtotal()}.00</div>
+              </div>
+              <div className="sub-total-child">
+                <div className="subtotal-label">Shipping</div>
+                <div className="subtotal">₹0.00</div>
+              </div>
+            </div>
+            <div className="total-child" />
+            <div className="total-parent">
+              <div className="total-label">Total</div>
+              <div className="total-cost">₹{calculateFulltotal()}.00</div>
+            </div>
+          </div>
+          <button className="checkout-button">
+            <Link to="/checkoutform">
+              <b className="checkout-button-text">Proceed to Checkout </b>
             </Link>
           </button>
         </div>
       </div>
-
-      <div className="cart-container-cart-total">
-        <div className="cart-total-heading">
-          <div className="heading-cart-totals">Cart Totals</div>
-        </div>
-        <div className="cart-total-money">
-          <div className="sub-total-parent">
-            <div className="sub-total-child">
-              <div className="subtotal-label">Sub-total</div>
-              <div className="subtotal">₹{calculateSubtotal()}.00</div>
-            </div>
-            <div className="sub-total-child">
-              <div className="subtotal-label">Shipping</div>
-              <div className="subtotal">₹0.00</div>
-            </div>
-          </div>
-          <div className="total-child" />
-          <div className="total-parent">
-            <div className="total-label">Total</div>
-            <div className="total-cost">₹{calculateFulltotal()}.00</div>
-          </div>
-        </div>
-        <button className="checkout-button">
-          <Link to="/checkoutform">
-            <b className="checkout-button-text">Proceed to Checkout </b>
-          </Link>
-        </button>
-      </div>
-    </div>
+    </>
   );
 }
 
